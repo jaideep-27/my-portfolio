9451aab1f0343f5c570b28c7b58fad31f10c9e45
jaideep-27 <amarabadjayadeep@gmail.com>
2024-10-21 01:34:45 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/my-portfolio/', // Add your repository name here
+  build: {
+    outDir: 'dist', // Ensure this is set to dist
+  },
 });
