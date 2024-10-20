995919523bd3c18f9756f8ccdf3d8c4842b7c44f
jaideep-27 <amarabadjayadeep@gmail.com>
2024-10-21 01:18:23 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',
   server: {
     hmr: {
       overlay: false, // You can disable the overlay if you want
     },
+  },
+  build: {
+    outDir: 'dist', // Ensure this is set to dist
   },
 });
