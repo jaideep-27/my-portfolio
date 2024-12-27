431f4f9fe8939a128ad3765ce908357d78afd85a
Benjamin1219-glitch <116884122+Benjamin1219-glitch@users.noreply.github.com>
2024-12-28 01:18:21 +0530

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Nandakishore";
+            document.title = "Portfolio | Jaideep Amrabad";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
