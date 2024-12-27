4e9ad79458fd54465b78f893ab44c8aaebd41a22
Benjamin1219-glitch <116884122+Benjamin1219-glitch@users.noreply.github.com>
2024-12-27 23:57:18 +0530

    Updates

--- projects/script.js
+++ projects/script.js
@@ -1,152 +1,49 @@
-$(document).ready(function () {
+// Header scroll
+let nav = document.querySelector("nav");
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
 
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
-    });
+// Menu toggle
+$('#menu').click(function () {
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
 });
 
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "Projects | Jaideep's Portfolio";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
-        }
-    });
+// Hide menu on scroll
+$(window).on('scroll load', function () {
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
 
-// Projects
-let projectsContainer = document.querySelector(".work .box-container");
-projectsContainer.innerHTML = `
-    <div class="grid-item basicweb">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-            <img draggable="false" src="https://jaideep-portfolio.netlify.app/assets/images/projects/portfolio1.png" alt="Portfolio Website" />
-            <div class="content">
-                <div class="tag">
-                    <h3>Portfolio Website</h3>
-                </div>
-                <div class="desc">
-                    <p>Personal Portfolio Website. Don't Need Much Info About It, Just Scroll Down. You're Here Only!</p>
-                    <div class="btns">
-                        <a href="#" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                        <a href="https://github.com/jaideep-27/my-portfolio" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+});
 
-    <div class="grid-item mern">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-            <img draggable="false" src="https://jaideep-portfolio.netlify.app/assets/images/projects/skillset.png" alt="SkillsetAI" />
-            <div class="content">
-                <div class="tag">
-                    <h3>SkillsetAI</h3>
-                </div>
-                <div class="desc">
-                    <p>Skillset AI transforms education with visual learning, gamification, and personalized tools, making learning engaging and effective.</p>
-                    <div class="btns">
-                        <a href="https://skillset-ai.netlify.app/" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                        <a href="https://github.com/jaideep-27/skillset-ai" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+// Tab title change on visibility
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState === "visible") {
+        document.title = "Projects | Jaideep's Portfolio";
+        $("#favicon").attr("href", "../assets/images/favicon.png");
+    }
+    else {
+        document.title = "Come Back To Portfolio";
+        $("#favicon").attr("href", "../assets/images/favhand.png");
+    }
+});
 
-    <div class="grid-item mern">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-            <img draggable="false" src="https://jaideep-portfolio.netlify.app/assets/images/projects/streamvibe.png" alt="StreamVibe" />
-            <div class="content">
-                <div class="tag">
-                    <h3>StreamVibe</h3>
-                </div>
-                <div class="desc">
-                    <p>A modern video sharing platform built with React, Node.js, MongoDB, and Cloudinary integration.</p>
-                    <div class="btns">
-                        <a href="https://streamvibe1.netlify.app/" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                        <a href="https://github.com/jaideep-27/streamvibe" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="grid-item basicweb">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-            <img draggable="false" src="https://jaideep-portfolio.netlify.app/assets/images/projects/ne.png" alt="Nakshatra Explorers" />
-            <div class="content">
-                <div class="tag">
-                    <h3>Nakshatra Explorers</h3>
-                </div>
-                <div class="desc">
-                    <p>A platform to explore the beauty of stars and learn about different constellations and celestial events.</p>
-                    <div class="btns">
-                        <a href="https://jaideep-27.github.io/Nakshatra_Explorers/" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                        <a href="https://github.com/jaideep-27/Nakshatra_Explorers" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="grid-item basicweb">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-            <img draggable="false" src="https://jaideep-portfolio.netlify.app/assets/images/projects/dreampath.png" alt="Dream Path" />
-            <div class="content">
-                <div class="tag">
-                    <h3>Dream Path</h3>
-                </div>
-                <div class="desc">
-                    <p>An AI web application that helps students find their ideal career path using AI.</p>
-                    <div class="btns">
-                        <a href="https://dreampath-cw.my.canva.site/" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                        <a href="https://github.com/jaideep-27/dreampath" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="grid-item android">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-            <img draggable="false" src="https://jaideep-portfolio.netlify.app/assets/images/projects/ecocheck.png" alt="EcoCheck" />
-            <div class="content">
-                <div class="tag">
-                    <h3>EcoCheck</h3>
-                </div>
-                <div class="desc">
-                    <p>An application in development.</p>
-                    <div class="btns">
-                        <a href="#" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                        <a href="#" class="btn" target="_blank"><i class="fas fa-code"></i> Code</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-`;
-
-// vanilla tilt.js
+// Initialize Vanilla Tilt
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 20,
 });
 
-// isotope filter products
+// Initialize Isotope for filtering
 var $grid = $('.box-container').isotope({
     itemSelector: '.grid-item',
     layoutMode: 'fitRows',
@@ -155,29 +52,19 @@ var $grid = $('.box-container').isotope({
     }
 });
 
-// filter items on button click
-$('.button-group').on('click', 'button', function () {
+// Filter items on button click
+$('.button-group').on('click', 'button', function() {
     $('.button-group').find('.is-checked').removeClass('is-checked');
     $(this).addClass('is-checked');
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
 });
 
-// disable developer mode
-document.onkeydown = function (e) {
-    if (e.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
+// Disable developer tools shortcuts
+document.onkeydown = function(e) {
+    if(e.keyCode == 123) return false;
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) return false;
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false;
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
 }
